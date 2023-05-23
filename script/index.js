d8c840512e8c3d89101a3dd822be7ee946491060
bautista <bautistamuchiutti@gmail.com>
2023-05-23 01:40:06 -0300

    mi primer js

---
+++ script/index.js
@@ -0,0 +1,11 @@
+let nombre= "nombre";
+let apellido= "apellido";
+console.log(nombre+ " " +apellido);
+
+let num = 6;
+let nom = 4;
+let suma = (num + nom);
+console.log(suma);
+let resultado = (suma + 2);
+console.log(resultado);
+
